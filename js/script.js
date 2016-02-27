7bb9b6ea0fba720e1ed2c6cd859b9a17a26e8959
Chuwei <chuwei@Chu-Weis-MacBook-Pro.local>
2016-02-27 10:08:33 -0600

    Add NYT API

--- js/script.js
+++ js/script.js
@@ -22,6 +22,32 @@ function loadData() {
     var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $address + '';
     $body.append('<img class= "bgimg" src="' +streetviewURL+ '">');
 
+
+    var nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+$city+'&sort=newest&api-key=f91f0ed94a941cd1678ff66126dcbb75:4:74551472'; 
+
+
+    $.getJSON(nytURL, function(data){
+        console.log(data);
+        console.log(nytURL);
+        $nytHeaderElem.text('Article about ' +$city);
+
+        var articles = data.response.docs;
+        for (var i=0; i<articles.length; i++){
+            var article = articles[i];
+            $nytElem.append('<li class = "article">'+
+                '<a href = "'+ article.web_url +'">'+ article.headline.main+'</a>'+
+                '<p>'+article.snippet+'</p>'+'</li>')
+        }
+        // $.each(data, function(webURL,snippet){
+        //     $nytElem.append('<li class = "article">'+'<a href = "'+ articles.web_url +'">'+ articles.headline.main+'</li>')
+        // });
+
+        // $('#nytimes-articles',{
+        //     html: items.join("")
+        // }).appendTo('body');
+
+    });
+
     return false;
 };
 
